c8e4086688fe5e055e1867b88d00c0308e683215
Nhoj <nhojplummer@yahoo.com>
2023-09-08 17:44:46 -0400

    Trying stuff

--- src/components/CardData/CardData.js
+++ src/components/CardData/CardData.js
@@ -1,8 +1,16 @@
 import React from "react"
 import { CardSection } from "../../containers/indexcon"
+import "./carddata.css"
+
+
+const cardData = [{}]
 
 const CardData = () => {
-  return <CardSection>CardData</CardSection>
+  return (
+    <CardSection>
+      <div className="card-data_contianer"></div>
+    </CardSection>
+  )
 }
 
 export default CardData
--- src/containers/CardSection/CardSection.js
+++ src/containers/CardSection/CardSection.js
@@ -2,7 +2,7 @@ import React from "react"
 import "./cardsection.css"
 
 const CardSection = props => {
-  return <div className="card-section_container">{props.children}</div>
+  return <div className="card-section_container section-padding">{props.children}</div>
 }
 
 export default CardSection
