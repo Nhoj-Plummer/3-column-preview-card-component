3119049cd83e409729679a95e66724c3f32f599a
Nhoj <nhojplummer@yahoo.com>
2023-09-08 19:02:41 -0400

    Trying more stuff

--- src/components/CardData/CardData.js
+++ src/components/CardData/CardData.js
@@ -1,8 +1,15 @@
 import React from "react"
 import "./carddata.css"
 
-const CardData = props => {
-  return <div className="card-data_contianer"></div>
+const CardData = ({ image, header, text }) => {
+  return (
+    <div className="card-data_contianer">
+      <img src={image} alt="icon" />
+      <h1>{header}</h1>
+      <p>{text}</p>
+      <button>Learn More</button>
+    </div>
+  )
 }
 
 export default CardData
--- src/containers/CardSection/CardSection.js
+++ src/containers/CardSection/CardSection.js
@@ -2,12 +2,12 @@ import React from "react"
 import "./cardsection.css"
 import Sedans from "../../assets/icon-sedans.svg"
 import SUVS from "../../assets/icon-suvs.svg"
-import Luxurt from "../../assets/icon-luxury.svg"
+import Luxury from "../../assets/icon-luxury.svg"
 import CardData from "../../components/CardData/CardData"
 
 const cardData = [
   {
-    img: { Sedans },
+    image: { Sedans },
     header: "SEDANS",
     text: "Choose a sedan for its affordability and excellent fuel economoy. Ideal for crusiing in the city or on your next road trip."
   }
@@ -16,7 +16,7 @@ const cardData = [
 const CardSection = () => {
   return (
     <div className="card-section_container section-padding">
-      <CardData />
+      <CardData image={Sedans} header="SEDANS" text="Choose a sedan for its affordability and excellent fuel economoy. Ideal for crusiing in the city or on your next road trip." />
     </div>
   )
 }
