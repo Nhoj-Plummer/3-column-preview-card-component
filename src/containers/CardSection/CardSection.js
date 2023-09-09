31d71c50abdb11c71a781dc42b14b5bd2226ee3d
Nhoj <nhojplummer@yahoo.com>
2023-09-09 16:19:01 -0400

    Making ideas near me

--- src/containers/CardSection/CardSection.js
+++ src/containers/CardSection/CardSection.js
@@ -16,8 +16,8 @@ import CardData from "../../components/CardData/CardData"
 const CardSection = () => {
   return (
     <div className="card-section_container section-padding">
-      <CardData image={Sedans} header="SEDANS" text="Choose a sedan for its affordability and excellent fuel economoy. Ideal for crusiing in the city or on your next road trip." />
-      <CardData image={SUVS} header="SUVS" text="Take an SUVS for its spacious interior, power. and versatility. Perfect for your next family vacation and off-road adventures." />
+      <CardData image={Sedans} header="SEDANS" text="Choose a sedan for its affordability and excellent fuel economoy. Ideal for cruising in the city or on your next road trip." />
+      <CardData image={SUVS} header="SUVS" text="Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures." />
       <CardData image={Luxury} header="LUXURY" text="Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style." />
     </div>
   )
